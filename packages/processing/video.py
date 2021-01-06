feb149c22c5137f8b8b2cb831849f71c2dcbb66f
carbajal <carbajal@informatik.uni-hamburg.de>
2021-01-06 11:24:06 +0100

    visualization: video + audio + label

---
+++ packages/processing/video.py
@@ -0,0 +1,18 @@
+from scipy.fftpack import idct
+import cv2
+import numpy as np
+
+def preprocess_ntcd_matlab(video_writer, matlab_frames, frame, width, height, y_hat_hard=None):
+    data_frame = matlab_frames[frame]  # data frame will be shortened to "df" below
+    reshaped_df = data_frame.reshape(width, height)
+    idct_df = idct(idct(reshaped_df).T).T
+    # normalized_df = idct_df / (matlab_frames_list_per_user.flatten().max() + epsilon) * 255.0
+    normalized_df = cv2.normalize(idct_df, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    rotated_df = np.rot90(normalized_df, 3)
+
+    # Add label on the video
+    if y_hat_hard[frame] == 1:
+        rotated_df[-9:,-9:] = 9*[255] # white square
+    
+    rgb_rotated_df = cv2.merge([rotated_df] * 3)
+    video_writer.write(rgb_rotated_df)
--- scripts/visualization_video.py
+++ scripts/visualization_video.py
@@ -5,13 +5,16 @@ import os
 import numpy as np
 import torch
 import h5py  # to read .mat files
-from scipy.fftpack import idct
 import math
 from tqdm import tqdm
 import cv2
 import ffmpeg
+import time
+import concurrent.futures # for multiprocessing
+import soundfile as sf
 
 from packages.processing.stft import stft
+from packages.processing.video import preprocess_ntcd_matlab
 
 # Dataset
 dataset_name = 'ntcd_timit'
@@ -47,7 +50,7 @@ pad_at_end = True  # pad audio file at end to match same size after stft + istft
 dtype = 'complex64' # STFT data type
 
 ## IBM
-quantile_fraction = 0.93
+quantile_fraction_begin = 0.93
 quantile_fraction_end = 0.99
 quantile_weight = 0.999
 
@@ -72,6 +75,73 @@ classif_dir = os.path.join('models', classif_name + '.pt')
 classif_data_dir = 'data/' + dataset_size + '/models/' + classif_name + '/'
 output_data_dir = os.path.join('data', dataset_size, 'models', classif_name + '/')
 
+#############################################################################################
+
+def process_video(args):
+    # Separate args
+    mat_file_path, audio_file_path  = args[0], args[1]
+
+    # Read clean speech
+    speech, fs_speech = sf.read(input_video_dir + audio_file_path, samplerate=None)
+
+    # Normalize audio
+    speech = speech/(np.max(np.abs(speech)))
+
+    # TF reprepsentation
+    speech_tf = stft(speech,
+                     fs=fs,
+                     wlen_sec=wlen_sec,
+                     win=win, 
+                     hop_percent=hop_percent,
+                     center=center,
+                     pad_mode=pad_mode,
+                     pad_at_end=pad_at_end,
+                     dtype=dtype) # shape = (freq_bins, frames)
+
+    # Compute vad
+    speech_vad = noise_robust_clean_speech_VAD(speech_tf,
+                                        quantile_fraction_begin=quantile_fraction_begin,
+                                        quantile_fraction_end=quantile_fraction_end,
+                                        quantile_weight=quantile_weight)
+    # Reduce dims
+    y_hat_hard = speech_vad[0]
+
+    # Read video
+    with h5py.File(input_video_dir + mat_file_path, 'r') as f:
+        for key, value in f.items():
+            matlab_frames_list_per_user = np.array(value)
+
+    # Process video
+    # initialize video writer
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    
+    video_filename = classif_data_dir + mat_file_path
+    video_filename = os.path.splitext(video_filename)[0] + '.mp4'
+    
+    if not os.path.exists(os.path.dirname(video_filename)):
+        os.makedirs(os.path.dirname(video_filename))
+
+    out = cv2.VideoWriter(video_filename, fourcc, visual_frame_rate, (width, height))
+    
+    # TODO: in parallel ?
+    for frame in range(matlab_frames_list_per_user.shape[0]):
+        preprocess_ntcd_matlab(video_writer=out,
+                                matlab_frames=matlab_frames_list_per_user,
+                                frame=frame,
+                                width=width,
+                                height=height,
+                                y_hat_hard=y_hat_hard)
+            
+    # close out the video writer
+    out.release()
+
+    # Add the audio using ffmpeg-python
+    video = ffmpeg.input(video_filename)
+    audio = ffmpeg.input(input_video_dir + audio_file_path)
+    out = ffmpeg.output(video, audio, os.path.splitext(video_filename)[0] + '_audio.mp4', vcodec='copy', acodec='aac', strict='experimental')
+    out = out.overwrite_output()
+    out.run()
+
 def main():
 
     # Create file list
@@ -81,79 +151,20 @@ def main():
     audio_file_paths = speech_list(input_speech_dir=input_video_dir,
                              dataset_type=dataset_type)
 
-    # TODO: in parallel
-    for i, (video_file_path, audio_file_path) in tqdm(enumerate(zip(mat_file_paths, audio_file_paths))):
+    t1 = time.perf_counter()
 
-        # Read video
-        with h5py.File(input_video_dir + video_file_path, 'r') as f:
-            for key, value in f.items():
-                matlab_frames_list_per_user = np.array(value)
+    args = [[mat_file_path, audio_file_path]
+                    for mat_file_path, audio_file_path in zip(mat_file_paths, audio_file_paths)]
 
-        # Process video
-        # initialize video writer
-        fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-        
-        video_filename = classif_data_dir + video_file_path
-        video_filename = os.path.splitext(video_filename)[0] + '.mp4'
-        
-        if not os.path.exists(os.path.dirname(video_filename)):
-            os.makedirs(os.path.dirname(video_filename))
+    with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
+        executor.map(process_video, args)
 
-        out = cv2.VideoWriter(video_filename, fourcc, visual_frame_rate, (width, height))
-        
-        # TODO: in parallel ?
-        for frame in range(matlab_frames_list_per_user.shape[0]):
-            data_frame = matlab_frames_list_per_user[frame]  # data frame will be shortened to "df" below
-            reshaped_df = data_frame.reshape(67, 67)
-            idct_df = idct(idct(reshaped_df).T).T
-            # normalized_df = idct_df / (matlab_frames_list_per_user.flatten().max() + epsilon) * 255.0
-            normalized_df = cv2.normalize(idct_df, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-            rotated_df = np.rot90(normalized_df, 3)
-            rgb_rotated_df = cv2.merge([rotated_df] * 3)
-            out.write(rgb_rotated_df)
-                
-        # close out the video writer
-        out.release()
-
-        # Add the audio using ffmpeg-python
-        video = ffmpeg.input(video_filename)
-        audio = ffmpeg.input(input_video_dir + audio_file_path)
-        out = ffmpeg.output(video, audio, os.path.splitext(video_filename)[0] + '_audio.mp4', vcodec='copy', acodec='aac', strict='experimental')
-        out = out.overwrite_output()
-        out.run()
-
-        # call(["ffmpeg", "-y", "-i", "{}{}/{}/{}.mp4".format(base_path, "test", speaker_folder, video_file[:-4]), 
-        # 	"-i", "{}{}/straightcam/{}.wav".format(audio_path, speaker_folder, video_file[:-4]), "-c:v", 
-        # 	"copy", "-c:a", "aac", "{}{}/{}-{:.2f}.mp4".format(estimation_path,  speaker_folder, video_file[:-4], _accuracy)])
-
-        # Read clean speech
-        #TODO:
-
-        # # TF reprepsentation
-        # speech_tf = stft(speech,
-        #                  fs=fs,
-        #                  wlen_sec=wlen_sec,
-        #                  win=win, 
-        #                  hop_percent=hop_percent,
-        #                  center=center,
-        #                  pad_mode=pad_mode,
-        #                  pad_at_end=pad_at_end,
-        #                  dtype=dtype) # shape = (freq_bins, frames)
-
-        # # Compute vad
-        # speech_vad = noise_robust_clean_speech_VAD(speech_tf,
-        #                                     quantile_fraction_begin=vad_quantile_fraction_begin,
-        #                                     quantile_fraction_end=vad_quantile_fraction_end,
-        #                                     quantile_weight=quantile_weight)
-
-
-# t1 = time.perf_counter()
-
-# with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
-#     all_metrics = executor.map(compute_metrics_utt, args)
-
-# t2 = time.perf_counter()
-# print(f'Finished in {t2 - t1} seconds')
+    # # TODO: in parallel
+    # for i, args in enumerate(zip(mat_file_paths, audio_file_paths)):
+    #     process_video(args)
+    
+    t2 = time.perf_counter()
+    print(f'Finished in {t2 - t1} seconds')
 
 if __name__ == '__main__':
     main()
