e52f906e96787443c68ab12dda52cacce8180dac
carbajal <carbajal@informatik.uni-hamburg.de>
2021-01-11 12:05:07 +0100

    create_video_train_set with dataset statisitics

--- packages/processing/video.py
+++ packages/processing/video.py
@@ -2,32 +2,22 @@ from scipy.fftpack import idct
 import cv2
 import numpy as np
 
-def preprocess_ntcd_matlab(video_writer, matlab_frames, frame, width, height, y_hat_hard=None):
+def preprocess_ntcd_matlab(matlab_frames, frame, width, height, y_hat_hard=None, video_writer=None):
     data_frame = matlab_frames[frame]  # data frame will be shortened to "df" below
     reshaped_df = data_frame.reshape(width, height)
     idct_df = idct(idct(reshaped_df).T).T
-    # normalized_df = idct_df / (matlab_frames_list_per_user.flatten().max() + epsilon) * 255.0
+    # normalized_df = idct_df / (matlab_frames.flatten().max() + 1e-8) * 255.0
     normalized_df = cv2.normalize(idct_df, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
     rotated_df = np.rot90(normalized_df, 3)
 
-    # Add label on the video
-    if y_hat_hard is not None and y_hat_hard[frame] == 1:
-        rotated_df[-9:,-9:] = 9*[255] # white square
-    
-    rgb_rotated_df = cv2.merge([rotated_df] * 3)
-    video_writer.write(rgb_rotated_df)
+    if video_writer is not None:
+        # Add label on the video
+        if y_hat_hard is not None and y_hat_hard[frame] == 1:
+            rotated_df[-9:,-9:] = 9*[255] # white square
 
-def preprocess_ntcd_matlab_nowriter(matlab_frames, frame, width, height, y_hat_hard=None):
-    data_frame = matlab_frames[frame]  # data frame will be shortened to "df" below
-    reshaped_df = data_frame.reshape(width, height)
-    idct_df = idct(idct(reshaped_df).T).T
-    # normalized_df = idct_df / (matlab_frames_list_per_user.flatten().max() + epsilon) * 255.0
-    normalized_df = cv2.normalize(idct_df, None, 255, 0, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    rotated_df = np.rot90(normalized_df, 3)
+        rgb_rotated_df = cv2.merge([rotated_df] * 3)
 
-    # Add label on the video
-    if y_hat_hard is not None and y_hat_hard[frame] == 1:
-        rotated_df[-9:,-9:] = 9*[255] # white square
-    
-    rgb_rotated_df = cv2.merge([rotated_df] * 3)
-    return rgb_rotated_df
+        video_writer.write(rgb_rotated_df)
+    else:
+        rgb_rotated_df = cv2.merge([rotated_df] * 3)
+        return rgb_rotated_df
--- scripts/create_video_train_set.py
+++ scripts/create_video_train_set.py
@@ -15,7 +15,7 @@ from packages.processing.target import noise_robust_clean_speech_VAD # because c
 # Parameters
 dataset_name = 'ntcd_timit'
 if dataset_name == 'ntcd_timit':
-    from packages.dataset.ntcd_timit_dataset import speech_list, write_dataset
+    from packages.dataset.ntcd_timit import video_list, speech_list
 
 ## Dataset
 dataset_types = ['train', 'validation']
@@ -28,17 +28,23 @@ output_data_folder = 'export'
 labels = 'vad_labels'
 # labels = 'ibm_labels'
 
+## Video
+# visual_frame_rate = 29.970030  # initial visual frames per second
+visual_frame_rate = 30 # initial visual frames per second
+width = 67
+height = 67
+
 ## STFT
-video_frame_rate = 29.970030  # frames per second
 fs = int(16e3) # Sampling rate
 wlen_sec = 64e-3 # window length in seconds
-hop_percent = math.floor((1 / (wlen_sec * video_frame_rate)) * 1e4) / 1e4  # hop size as a percentage of the window length
+hop_percent = math.floor((1 / (wlen_sec * visual_frame_rate)) * 1e4) / 1e4  # hop size as a percentage of the window length
 # hop_percent = 0.25
 win = 'hann' # type of window
 center = False # see https://librosa.org/doc/0.7.2/_modules/librosa/core/spectrum.html#stft
 pad_mode = 'reflect' # This argument is ignored if center = False
 pad_at_end = True # pad audio file at end to match same size after stft + istft
 dtype = 'complex64'
+
 
 ## Noise robust VAD
 vad_quantile_fraction_begin = 0.93
@@ -55,10 +61,10 @@ rdcc_nslots = 1e5 # The number of slots in the cache's hash table
                   # (see https://docs.h5py.org/en/stable/high/file.html?highlight=rdcc#chunk-cache)
                   # for compression 'zlf' --> 1e4 - 1e7
                   # for compression 32001 --> 1e4
-# TODO: modify chunks
-X_shape = (67, 67, 0)
-X_maxshape = (67, 67, None)
-X_chunks = (67, 67, 1)
+
+X_shape = (width, height, 3, 0)
+X_maxshape = (width, height, 3, None)
+X_chunks = (width, height, 3, 1)
 
 Y_shape = (1, 0)
 Y_maxshape = (1, None)
@@ -76,6 +82,11 @@ def main():
         os.makedirs(os.path.dirname(output_dataset_file))
 
     with h5.File(output_dataset_file, 'a', rdcc_nbytes=rdcc_nbytes, rdcc_nslots=rdcc_nslots) as f:    
+
+        # video attributes
+        f.attrs['visual_frame_rate'] = visual_frame_rate
+        f.attrs['width'] = width
+        f.attrs['height'] = height
             
         # STFT attributes
         f.attrs['fs'] = fs
@@ -104,7 +115,10 @@ def main():
         for dataset_type in dataset_types:
 
             # Create file list
-            file_paths = speech_list(input_video_dir=input_video_dir,
+            mat_file_paths = video_list(input_video_dir=input_video_dir,
+                                    dataset_type=dataset_type)
+            
+            audio_file_paths = speech_list(input_speech_dir=input_video_dir,
                                     dataset_type=dataset_type)
 
             # Delete datasets if already exists
@@ -126,27 +140,19 @@ def main():
             if dataset_type == 'train':
                 # VAR = E[X**2] - E[X]**2
                 channels_sum, channels_squared_sum = 0., 0.
+                # channels_sum = np.zeros((width, height, 3), dtype='float32')
+                # channels_squared_sum = np.zeros_like(channels_sum)
 
-            for i, file_path in tqdm(enumerate(file_paths)):
+            for i, (mat_file_path, audio_file_path) in tqdm(enumerate(zip(mat_file_paths, audio_file_paths))):
 
-                speech, fs_speech = sf.read(input_video_dir + file_path, samplerate=None)
-
-                # Normalize audio
-                speech = speech/(np.max(np.abs(speech)))
+                # Read clean speech
+                speech, fs_speech = sf.read(input_video_dir + audio_file_path, samplerate=None)
 
                 if fs != fs_speech:
                     raise ValueError('Unexpected sampling rate')
 
-                if dataset_size == 'subset':
-                    # Save .wav files, just to check if it working
-                    output_path = output_video_dir + file_path
-                    output_path = os.path.splitext(output_path)[0]
-
-                    if not os.path.exists(os.path.dirname(output_path)):
-                        os.makedirs(os.path.dirname(output_path))
-                    
-                    sf.write(output_path + '_s.wav', speech, fs)
-                    # sf.write(output_path + '_x.wav', mixture, fs)
+                # Normalize audio
+                speech = speech/(np.max(np.abs(speech)))
 
                 # TF reprepsentation
                 speech_tf = stft(speech,
@@ -158,19 +164,48 @@ def main():
                                 pad_mode=pad_mode,
                                 pad_at_end=pad_at_end,
                                 dtype=dtype) # shape = (freq_bins, frames)
-                spectrogram = np.power(abs(speech_tf), 2)
+
+                if dataset_size == 'subset':
+                    # Save .wav files, just to check if it working
+                    output_path = output_video_dir + audio_file_path
+                    output_path = os.path.splitext(output_path)[0]
+
+                    if not os.path.exists(os.path.dirname(output_path)):
+                        os.makedirs(os.path.dirname(output_path))
+                    
+                    sf.write(output_path + '_s.wav', speech, fs)
+                    # sf.write(output_path + '_x.wav', mixture, fs)
+
+                # Read video
+                with h5.File(input_video_dir + mat_file_path, 'r') as vfile:
+                    for key, value in vfile.items():
+                        matlab_frames_list_per_user = np.array(value)
+
+                # Init video
+                frames = matlab_frames_list_per_user.shape[0]
+                video = np.zeros((width, height, 3, frames), dtype='float32')
+                
+                # Process video                
+                for j, frame in enumerate(range(frames)):
+                    video[..., j] = preprocess_ntcd_matlab(video_writer=None,
+                                                          matlab_frames=matlab_frames_list_per_user,
+                                                          frame=frame,
+                                                          width=width,
+                                                          height=height,
+                                                          y_hat_hard=None)
 
                 # Store spectrogram in dataset
-                fx.resize((fx.shape[1] + spectrogram.shape[1]), axis = 1)
-                fx[:,-spectrogram.shape[1]:] = spectrogram
+                fx.resize((fx.shape[-1] + video.shape[-1]), axis = len(fx.shape)-1)
+                fx[...,-video.shape[-1]:] = video
 
 
                 if labels == 'vad_labels':
-                    # vad
+                    # Compute vad
                     speech_vad = noise_robust_clean_speech_VAD(speech_tf,
                                                         quantile_fraction_begin=vad_quantile_fraction_begin,
                                                         quantile_fraction_end=vad_quantile_fraction_end,
-                                                        quantile_weight=vad_quantile_weight)                    
+                                                        quantile_weight=vad_quantile_weight)
+
                     label = speech_vad
 
                 if labels == 'ibm_labels':
@@ -183,20 +218,21 @@ def main():
                     label = speech_ibm
 
                 # Store spectrogram in label
-                fy.resize((fy.shape[1] + label.shape[1]), axis = 1)
-                fy[:,-label.shape[1]:] = label
+                fy.resize((fy.shape[-1] + label.shape[-1]), axis = len(fy.shape)-1)
+                fy[...,-label.shape[-1]:] = label
 
                 # Compute mean, std
                 if dataset_type == 'train':
                     # VAR = E[X**2] - E[X]**2
-                    channels_sum += np.sum(spectrogram, axis=-1)
-                    channels_squared_sum += np.sum(spectrogram**2, axis=-1)
+                    channels_sum += np.sum(video, axis=-1)
+                    channels_squared_sum += np.sum(video**2, axis=-1)
             
             # Compute and save mean, std
             if dataset_type == 'train':
                 print('Compute mean and std')
                 #NB: compute the empirical std (!= regular std)
-                n_samples = fx.shape[1]
+                n_samples = fx.shape[-1]
+                # n_samples = np.prod(fx.shape)
                 mean = channels_sum / n_samples
                 std = np.sqrt((1/(n_samples - 1))*(channels_squared_sum - n_samples * mean**2))
                 
